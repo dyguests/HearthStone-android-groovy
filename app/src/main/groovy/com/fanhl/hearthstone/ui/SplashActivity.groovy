8a04844dcdd518c412a949cd062e396cffe04fe5
fanhl <Dyguest0315>
2015-02-22 18:02:16 +0800

    重构整个项目

--- app/src/main/groovy/com/fanhl/hearthstone/ui/SplashActivity.groovy
+++ app/src/main/groovy/com/fanhl/hearthstone/ui/SplashActivity.groovy
@@ -1,41 +1,32 @@
-package com.fanhl.hearthstone.ui;
+package com.fanhl.hearthstone.ui
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.ActionBarActivity
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.fanhl.hearthstone.R
 
-import com.fanhl.hearthstone.R;
-
-
-public class SplashActivity extends ActionBarActivity {
+public class SplashActivity extends AbstractBaseActivity {
+    Handler mHandler = new Handler()
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        super.onCreate(savedInstanceState)
+        View view = View.inflate(this, R.layout.activity_splash, null)
+        setContentView(view)
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.alpha)
+        view.startAnimation(animation)
+        animation.animationListener = [
+                onAnimationStart : {},
+                onAnimationRepeat: {},
+                onAnimationEnd   : { mHandler.postDelayed({ goHome() }, 500) }
+        ] as Animation.AnimationListener
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private void goHome() {
+        openActivity(MainActivity.class)
+        finish()
     }
 }
